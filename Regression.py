2360bcd9418c2839d4a6185b830d50746dd3a1aa
DavideV-git1 <64208630+DavideV-git1@users.noreply.github.com>
2021-09-02 19:49:10 +0200

    Update Regression.py

--- Regression.py
+++ Regression.py
@@ -3,6 +3,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 from sklearn.svm import SVR
+from sklearn.linear_model  import Ridge
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 
 class Regression:
@@ -21,6 +25,26 @@ class Regression:
         print('MSE train data: %.3f, MSE test data: %.3f' %
               (metrics.mean_squared_error(y_train_pred, self.y_train),
                metrics.mean_squared_error(y_test_pred, self.y_test)))
+        # plt.style.use('seaborn')
+        # plt.scatter(y_train_pred, y_test_pred, color='red', marker='o', s=35, alpha=0.5,
+        #             label='Test data')
+        # plt.plot(y_train_pred, y_train_pred, color='blue', label='Model Plot')
+        # plt.title('Predicted Values vs Inputs')
+        # plt.xlabel('Inputs')
+        # plt.ylabel('Predicted Values')
+        # plt.legend(loc='upper left')
+        # plt.show()
+
+        plt.scatter(y_train_pred, self.y_train)
+        plt.title('Linear Regression')
+        tmp = [min(np.concatenate((y_train_pred, y_test_pred))),
+               max(np.concatenate((y_train_pred, y_test_pred)))]
+        plt.plot(tmp, tmp, 'r')
+        plt.xlabel('Predicted Value')
+        plt.ylabel('Actual Value')
+        plt.show()
+
+
 
     def randomForest(self):
         rfr = RandomForestRegressor(n_estimators=100, criterion='mse', random_state=1, n_jobs=-1)
@@ -31,6 +55,20 @@ class Regression:
         print('MSE train data: %.3f, MSE test data: %.3f' %
               (metrics.mean_squared_error(y_train_pred, self.y_train),
                metrics.mean_squared_error(y_test_pred, self.y_test)))
+        plt.figure(figsize=(8, 6))
+        plt.title('Random Forest')
+        plt.scatter(y_train_pred, y_train_pred - self.y_train,
+                    c='gray', marker='o', s=35, alpha=0.5,
+                    label='Train data')
+        plt.scatter(y_test_pred, y_test_pred - self.y_test,
+                    c='blue', marker='o', s=35, alpha=0.7,
+                    label='Test data')
+        plt.xlabel('Predicted values')
+        plt.ylabel('Actual values')
+        plt.legend(loc='upper right')
+        plt.hlines(y=0, xmin=0, xmax=60000, lw=2, color='red')
+        plt.show()
+
 
     def supportVectorRegression(self):
         svr = SVR(kernel='rbf')
@@ -41,14 +79,41 @@ class Regression:
         print('MSE train data: %.3f, MSE test data: %.3f' %
               (metrics.mean_squared_error(y_train_pred, self.y_train),
                metrics.mean_squared_error(y_test_pred, self.y_test)))
+        plt.title('SVR')
+        plt.scatter(y_train_pred, self.y_train)
+        tmp = [min(np.concatenate((y_train_pred, y_test_pred))),
+               max(np.concatenate((y_train_pred, y_test_pred)))]
+        plt.plot(tmp, tmp, 'r')
+        plt.xlabel('Predicted Value')
+        plt.ylabel('Actual Value')
+        plt.show()
+
 
     def KRLS(self):
+        ridge = Ridge(alpha=0.5)
+        ridge.fit(self.x_train, self.y_train)
+        y_train_pred = ridge.predict(self.x_train)
+        y_test_pred = ridge.predict(self.x_test)
+        print("### RESULTS FOR KRLS ###")
+        print('MSE train data: %.3f, MSE test data: %.3f' %
+              (metrics.mean_squared_error(y_train_pred, self.y_train),
+               metrics.mean_squared_error(y_test_pred, self.y_test)))
+        plt.title('KRLS')
+        plt.scatter(y_train_pred, self.y_train)
+        tmp = [min(np.concatenate((y_train_pred, y_test_pred))),
+               max(np.concatenate((y_train_pred, y_test_pred)))]
+        plt.plot(tmp, tmp, 'r')
+        plt.xlabel('Predicted Value')
+        plt.ylabel('Actual Value')
+        plt.show()
+
         """
         TODO:
-        1. funzione KRLS
         2. selezione degli iperparametri dove richiesto tramite k-fold
         3. Scatter plot
         """
+
+
 
 
 
